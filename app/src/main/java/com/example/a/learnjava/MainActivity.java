797c384afe502df2e5743426479bf94595d9fd1a
The A <iagree.createmyaccount@gmail.com>
2018-02-13 23:16:52 +0300

    Add button action

--- app/src/main/java/com/example/a/learnjava/MainActivity.java
+++ app/src/main/java/com/example/a/learnjava/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.a.learnjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttStart;
+    Button buttFinish;
+    TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    
-    private class TestGit {
-        //nothing here
+        setContentView(R.layout.main);
+
+        buttStart = findViewById(R.id.buttStart);
+        buttFinish = findViewById(R.id.buttFinish);
+        showText = findViewById(R.id.tv);
+
+        View.OnClickListener startListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showText.setText("Goodbye world!");
+            }
+        };
+
+        buttStart.setOnClickListener(startListener);
+
     }
 }
